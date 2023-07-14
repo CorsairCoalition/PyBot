c041583cc167346b8b3f1dedff2990a2b39a93bf
Austin Davis <austindavis@gmail.com>
2023-07-14 00:26:47 -0400

    Improved Documentation

--- pythonbot.py
+++ pythonbot.py
@@ -8,6 +8,27 @@ from typing import final
 @final
 class Map:
 
+    """Manages all map-related data for a bot including terrain, armies, cities, etc.
+    Map objects are updated for each bot before each turn automatically by the framework
+    whenever a turn message is received from the server. The map object is accessible
+    through the game object, e.g. `game.map`.
+
+    Multiple helper methods are provided to make it easier to work with the map data.
+
+    Provides access to the following data:
+    - size: The total number of tiles on the map.
+    - height: The height of the map.
+    - width: The width of the map.
+    - enemyGeneral: The tile index of the enemy general.
+    - ownGeneral: The tile index of the bot's general.
+    - armies: A list of the armies on the map. Each entry is a tuple of the form (tile, strength).
+    - cities: A list of the cities on the map. Each entry is a tile index.
+    - discoveredTiles: A list of booleans indicating whether each tile has been discovered.
+    - enemyTiles: A list of lists of the enemy tiles. Each entry is a tuple of the form [tile, strength].
+    - ownTiles: A list of lists of the bot's tiles. Each entry is a list of tile indices.
+    - terrain: A list of the terrain on the map. Each entry is a tile index of the type __TILE__. 
+    """
+
     size:int
     height:int
     width:int
@@ -88,6 +109,12 @@ class Map:
 
 @final
 class GameInstance:
+    """Created whenever a game starts. Contains all the information about the game that is available to the bot.
+    Access the map with game.map, and the current turn with game.turn.
+
+    Args:
+        game_start_message (dict): The game_start message from the redis channel.
+    """
 
     playerIndex:int
     playerColors:list[int]
@@ -102,7 +129,6 @@ class GameInstance:
     
     def __init__(self, game_start_message:dict) -> None:
         game_start_message = literal_eval(game_start_message.decode())['game_start']
-        print(f"MADE HERE {game_start_message}")
         self.playerIndex= game_start_message['playerIndex']
         self.playerColors = game_start_message['playerColors']
         self.replay_id= game_start_message['replay_id']
@@ -112,20 +138,34 @@ class GameInstance:
         self.game_type = game_start_message['game_type']
         self.options=game_start_message['options']
 
-
-    def update(self,game_state_byte_data:dict) -> None:
-        game_state_data_dict = dict({k.decode():json.loads(v.decode()) for k,v in game_state_byte_data.items()})
-        self.turn = game_state_data_dict['turn']
-        self.map = Map(game_state_data_dict)
+    def update(self,game_state_data:dict) -> None:
+        """
+        Updates the game instance with the given game state data.
+        """
+        self.turn = game_state_data['turn']
+        self.map = Map(game_state_data)
 
 @final
 class Action:
     @classmethod
     def serialize(cls,start:int,end:int,is50:bool=False,interrupt:bool=False):
+        """Serializes a move for the game engine.
+
+        Args:
+            start (int): the tile to move from
+            end (int): the tile to move to
+            is50 (bool, optional): If true, only half of the army will be moved. Defaults to False.
+            interrupt (bool, optional): If True, the server-side movement queue will be cleared. Defaults to False.
+
+        Returns:
+            str: json representation of the movement action suitable to be passed to Redis
+        """
         return json.dumps({'interrupt':interrupt,'actions':[{'start':start, 'end':end, 'is50?':is50}]})
 
 @final
 class RedisChannelList:
+    """A data class to hold the redis channel names for a bot
+    """
     TURN:str
     ACTION:str
     RECOMMENDATION:str
@@ -138,6 +178,17 @@ class RedisChannelList:
         self.STATE = f"{bot_id}-state"
 
 class PythonBot:
+    """A base class for python bots. Subclass this to create a bot. 
+
+    Subclasses must implement the do_turn() method.
+
+    Raises:
+        NotImplementedError: if the do_turn() method is not implemented
+
+    Args:
+        bot_id: a unique identifier for the bot. Must be unique across all bots.
+    """
+
     game:GameInstance
     redis_connetion:redis.Redis
     pubsub:redis.client.PubSub
@@ -149,10 +200,20 @@ class PythonBot:
         self.channel_list = RedisChannelList(self.bot_id)
 
     def do_turn():
+        """Called every turn. Must be implemented by the bot subclass."""
         raise NotImplementedError("do_turn() must be implemented by the bot subclass.")
 
     @final
     def move(self, start:int, end:int, is50:bool=False,interrupt=False) -> None:
+        """
+        Moves armies from one tile to another.
+
+        Args:
+            start (int): the tile to move from
+            end (int): the tile to move to
+            is50 (bool, optional): If true, only half of the army will be moved. Defaults to False.
+            interrupt (bool, optional): If True, the server-side movement queue will be cleared. Defaults to False.
+        """
         self.redis_connetion.publish(self.channel_list.ACTION, Action.serialize(start,end,is50,interrupt))
 
     @final
@@ -163,7 +224,8 @@ class PythonBot:
     @final
     def __update_state__(self):
         update_data = self.redis_connetion.hgetall(f'{self.bot_id}-{self.game.replay_id}')
-        self.game.update(update_data)
+        game_state_data_dict = dict({k.decode():json.loads(v.decode()) for k,v in update_data.items()})
+        self.game.update(game_state_data_dict)
 
     @final
     def __is_game_start_message__(self, message: dict) -> bool:
@@ -190,49 +252,58 @@ class PythonBot:
         if self.__is_game_start_message__(message):
             self.game = GameInstance(message['data'])
 
-import os
 class RedisConnectionManager:
+    """This class will handle all Redis connections and subscriptions for Python bots. 
+    Register bots with the register() method. Individual bots will be notified when a 
+    game starts or when a turn is taken.
 
-    channel_map:dict[str, callable] = {}
+    Args:
+        redis_config_file (str, optional): The path to the Redis config file. Defaults to None.
+    """
+    __channel_map__:dict[str, callable] = {}
 
-    # Path to the Redis config file
-    redis_config_file:str = os.path.dirname(__file__) + '/../config.json'
-    redis_connection:redis.Redis
-    pubsub:redis.client.PubSub
+    __redis_connection__:redis.Redis
+    __pubsub__:redis.client.PubSub
 
-    def __init__(self):
-        config_file = json.load(open(self.redis_config_file))['redisConfig']
-        
-        self.redis_config = dict(
-            host=config_file['HOST'],
-            port=config_file['PORT'],
-            username=config_file['USERNAME'],
-            password=config_file['PASSWORD'],
-            ssl=False)
-        
+    def __init__(self,redis_config:dict) -> None:
         # Connect to Redis
-        self.redis_connection = redis.Redis(**(self.redis_config), client_name="Python_RedisConnectionManager")
+        self.__redis_connection__ = redis.Redis(**(redis_config), client_name="Python_RedisConnectionManager")
         
         # Subscribe to redis channel(s)
-        self.pubsub = self.redis_connection.pubsub()
-
-    def run(self):
-
-        # Start listening for Redis messages
-        for message in self.pubsub.listen():
-            try:
-                channel:str = message['channel'].decode()
-                self.channel_map[channel](message)
-            except KeyError:
-                print(f"Received unknown message type: {message}", file=sys.stderr)
+        self.__pubsub__ = self.__redis_connection__.pubsub()
 
     def __enter__(self):
         return self
     
     def __exit__(self, exc_type, exc_value, traceback):
-        self.redis_connection.close()
+        self.__redis_connection__.close()
+
+    def run(self):
+        """
+        This method will start listening for Redis messages. When a message is received,
+        the appropriate bot will be notified, and the message will be passed to the bot's
+        handler method. If it is a turn, the bot's do_turn() method will be called. This 
+        method will block until the connection is closed.
+        """
+
+        # Start listening for Redis messages
+        for message in self.__pubsub__.listen():
+            try:
+                channel:str = message['channel'].decode()
+                self.__channel_map__[channel](message)
+            except KeyError:
+                print(f"Received unknown message type: {message}", file=sys.stderr)
 
     def register(self, bot:PythonBot):
-        bot.__register_redis__(self.redis_connection, self.pubsub)
-        self.channel_map[bot.channel_list.TURN] = bot.__handle_turn_channel_message__
-        self.channel_map[bot.channel_list.STATE] = bot.__handle_state_channel_message__
+        """Registers the bot with this connection manager. All messages received on the bot's
+        channels will be passed to the bot's handler methods. 
+        This method also sets the bot's redis connection and pubsub objects to the ones used
+        by this connection manager; any messages sent by the bot (e.g., move commands) will 
+        be sent over this connection.
+
+        Args:
+            bot (PythonBot): the bot to be registered with this connection manager
+        """
+        bot.__register_redis__(self.__redis_connection__, self.__pubsub__)
+        self.__channel_map__[bot.channel_list.TURN] = bot.__handle_turn_channel_message__
+        self.__channel_map__[bot.channel_list.STATE] = bot.__handle_state_channel_message__
